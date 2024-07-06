8f37adc4f1a5bababae1f67e599c3cb7bef6a88d
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2024-07-07 03:17:42 +0600

    sort by view added

--- JS/courses.js
+++ JS/courses.js
@@ -7,41 +7,58 @@ const filterBtnDisplay = (categories) => {
         `
     });
 }
-
+let categoryId = 1000;
 const filterByID = (id) => {
+    categoryId = id;
     loadCourseByCategories(id);
 }
+console.log(categoryId);
 
-const courseDisplay = (data) => {
-    console.log(data);
+const courseDisplay = (data, sort) => {
+    console.log(sort);
+    if (sort === "sort") {
+        for (let i = 0; i < data.length - 1; i++) {
+            let highValueIndex = i;
+            let highValue = parseInt(data[i].others.views);
+
+            for (let j = i + 1; j < data.length; j++) {
+
+                if (highValue < parseInt(data[j].others.views)) {
+                    // console.log("i", parseInt(data[i].others.views), "j", parseInt(data[j].others.views));
+                    highValueIndex = j;
+                    highValue = parseInt(data[j].others.views);
+                }
+            }
+            [data[i], data[highValueIndex]] = [data[highValueIndex], data[i]];
+        }
+    }
+
     const parentDiv = document.getElementById("card-container");
     parentDiv.innerText = "";
-    if(data.length == 0){
+    if (data.length == 0) {
         const childDiv = document.createElement('div');
         childDiv.className = "text-center flex flex-col gap-11 mt-11 items-center";
-        childDiv.innerHTML = 
-        `
+        childDiv.innerHTML =
+            `
         <img class="" src="./images/icons/Icon.png" alt="" srcset="">
                 <h2 class="text-3xl font-bold">Oops!! Sorry, There is no content here</h2>
         `
         parentDiv.appendChild(childDiv)
     }
     data.forEach(item => {
-        console.log(item);
-        const hours =  parseInt(item?.others?.posted_date / 3600);
-        const min = parseInt((item?.others?.posted_date % 3600)/60)
-    
+        const hours = parseInt(item?.others?.posted_date / 3600);
+        const min = parseInt((item?.others?.posted_date % 3600) / 60)
+
         const childDiv = document.createElement('div');
         childDiv.className = "card rounded-md bg-base-100 w-80 shadow-md";
-        childDiv.innerHTML = 
-        
-        `
+        childDiv.innerHTML =
+
+            `
         <div class="hero h-44 rounded-md"
             style="background-image: url(${item?.thumbnail});">
             <div class="hero-overlay rounded-md bg-opacity-20"></div>
 
-            ${
-                (hours || min)?`<button class=" bg-black px-2 py-1 rounded-md text-white absolute right-2 top-36 text-[10px]">${hours} hrs ${min} min ago</button>`: `<button class=" bg-black px-2 py-1 rounded-md text-white absolute right-2 top-36 text-[10px]">few second ago</button>`
+            ${(hours || min) ? `<button class=" bg-black px-2 py-1 rounded-md text-white absolute right-2 top-36 text-[10px]">${hours} hrs ${min} min ago</button>` : `<button class=" bg-black px-2 py-1 rounded-md text-white absolute right-2 top-36 text-[10px]">few second ago</button>`
             }
         </div>
         <div class="pt-3 px-2 space-x-2 text-base font-bold flex justify-start items-center">
@@ -55,9 +72,8 @@ const courseDisplay = (data) => {
         <div class="py-3 pl-12 space-y-1">
             <div class="flex justify-start items-center space-x-2">
                 <p class="text-sm">${item?.authors[0]?.profile_name}</p>
-                ${
-                    (item?.authors[0]?.verified) ? `<img class=" w-[16px] h-[16px] rounded-full " src="./images/icons/verifed.png" alt="" srcset=""></img>`:""
-                }
+                ${(item?.authors[0]?.verified) ? `<img class=" w-[16px] h-[16px] rounded-full " src="./images/icons/verifed.png" alt="" srcset=""></img>` : ""
+            }
                 
             </div>
             <p class="text-sm">${item?.others?.views} views</p>
@@ -66,4 +82,8 @@ const courseDisplay = (data) => {
         `
         parentDiv.appendChild(childDiv);
     });
+}
+
+const filterByViews = () => {
+    loadCourseByCategories(categoryId, 'sort');
 }
--- JS/dataload.js
+++ JS/dataload.js
@@ -3,10 +3,12 @@ const categoriesLoad = async () => {
     const categories = await res.json();
     filterBtnDisplay(categories.data);
 }
-const loadCourseByCategories = async (id) => {
+const loadCourseByCategories = async (id,sort) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
     const data = await res.json();
-    courseDisplay(data.data);
+    courseDisplay(data.data,sort);
 }
 
+
 categoriesLoad()
+loadCourseByCategories(1000)
