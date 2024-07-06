fa0b32eece72f001a29985cc810a1249bfb666e8
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2024-07-06 22:13:26 +0600

    filtered data

--- JS/courses.js
+++ JS/courses.js
@@ -1,14 +1,17 @@
-const filterBtnDisplay = (categories)=>{
+const filterBtnDisplay = (categories) => {
     console.log(categories);
     const filterBtnDiv = document.getElementById('filter-btn-div');
     categories.forEach(category => {
-        console.log(category);
         filterBtnDiv.innerHTML += `
         <button onclick="filterByID(${category.category_id})" class="filter rounded px-6 py-1 text-white bg-primaryColor">${category.category}</button>
         `
     });
 }
 
-const filterByID = (id)=>{
-    console.log(id);
+const filterByID = (id) => {
+    loadCourseByCategories(id);
+}
+
+const courseDisplay = (data)=>{
+    console.log(data);
 }
--- JS/dataload.js
+++ JS/dataload.js
@@ -1,7 +1,12 @@
-const categoriesLoad = async()=>{
+const categoriesLoad = async () => {
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/categories`);
     const categories = await res.json();
     filterBtnDisplay(categories.data);
+}
+const loadCourseByCategories = async (id) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
+    const data = await res.json();
+    courseDisplay(data.data);
 }
 
 categoriesLoad()
